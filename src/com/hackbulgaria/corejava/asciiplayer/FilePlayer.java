1e14bb2ccfe0334afd46e6e027c257ffe19287d2
Velko Spasov <spasov.velko@gmail.com>
2014-07-03 21:37:17 +0300

    FilePlayer abstract class

---
+++ src/com/hackbulgaria/corejava/asciiplayer/FilePlayer.java
@@ -0,0 +1,13 @@
+package com.hackbulgaria.corejava.asciiplayer;
+
+import java.io.File;
+
+/**
+ * Created by kimusubi on 14-7-3.
+ */
+public class FilePlayer {
+
+    public FilePlayer(File file) {
+
+    }
+}
