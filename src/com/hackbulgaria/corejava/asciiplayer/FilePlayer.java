ac837fa56d196107451186e5c0f634b23b881f81
Velko Spasov <spasov.velko@gmail.com>
2014-07-03 21:48:31 +0300

    Abstract FilePlayer Class init

--- src/com/hackbulgaria/corejava/asciiplayer/FilePlayer.java
+++ src/com/hackbulgaria/corejava/asciiplayer/FilePlayer.java
@@ -5,9 +5,10 @@ import java.io.File;
 /**
  * Created by kimusubi on 14-7-3.
  */
-public class FilePlayer {
+public abstract class FilePlayer implements IPlayer {
+    protected File file;
 
-    public FilePlayer(File file) {
-
+    public FilePlayer(String path) {
+        file = new File(path);
     }
 }
